9a8dbcd05455ab8a41c5956e240e3c2452f376c4
Swaff <swaff68@gmail.com>
2014-06-27 15:23:01 -0600

    finished

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 $(document).on('ready', function() {
+ 
+
+var $el;
+
+$('button').click(function(){
+	$('form').show()
+	$('button').hide()
+});
+
+$('#submit').click(function(x) {
+	$el = $(this);
+	x.preventDefault()
+	// console.log($el)
+
+	var input = $('#nameInput').val()
+	$('#name').text(input)
+
+	var input = $('#bioInput').val()
+	$('#bio').text(input)
+
+	var input = $('#bookInput').val()
+	$('#favBooks').text(input)
+
+	var input = $('#jslInput').val()
+	$('#favLibs').text(input)
+
+	$('form').hide()
+	$('button').show()
+
+
+	});
+
   
 });
